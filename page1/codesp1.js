a8801eb69b1502e6a324658397603a335d8531dc
Muhammadqodir Abduraxmanov <60865598+Muhammad-uzbek@users.noreply.github.com>
2021-06-28 12:07:20 +0500

    Add files via upload

---
+++ page1/codesp1.js
@@ -0,0 +1,63 @@
+//const { image } = require("html2canvas/dist/types/css/types/image");
+
+let quote = document.querySelector("#quote");
+let author = document.querySelector(".author");
+let block = document.querySelector(".image-block");
+let btn= document.querySelector("#download");
+generate= document.querySelector("#generate");
+let changefont = document.querySelector("#changefont");
+let changeimg = document.querySelector("#changeimg");
+let img= document.querySelector("#realimage");
+let images = ['estetik.jpeg','estetik1.jpeg', 'night.jpeg', 'night1.jpeg', 'dusk.jpeg','night2.jpeg', 'sky1.jpeg'];
+
+async function randomQuote() {
+  const response = await fetch('https://api.quotable.io/random?maxLength=50')
+  const data = await response.json()
+  quote.innerHTML=`${data.content}`;
+  let dataAu= data.author;
+  if(dataAu.length<15){
+     author.innerHTML=data.author;
+  } else{
+      dataAu= data.author.split(" ");
+      author.innerHTML= dataAu[0].slice(0,1)+"."+dataAu[dataAu.length-1];
+      console.log(dataAu);
+  }
+}
+randomQuote()
+
+btn.addEventListener('click', ()=>{
+    html2canvas(block).then(canvas =>{
+        saveAs(canvas.toDataURL(), 'Photos')
+    })
+});
+
+function saveAs(url, fileName){
+    let link = document.createElement("a");
+    link.href = url;
+    link.download= fileName;
+    link.click();
+}
+changefont.addEventListener("click",()=>{
+    let fonts=['Helvetica', 'monospace','Brush Script MT', 'Times New Roman'];
+    quote.style.fontFamily= fonts[count(fonts.length)];
+});
+
+ changeimg.addEventListener("click",()=>{
+     img.src='../img/'+images[count(images.length-1)];
+ });
+// async function pixabay(){
+//     let url="https://pixabay.com/api/?key=22162484-3a43efc1ec849682073e5aa95&q=";
+//     let types=['nature', 'science', 'Aesthetic', 'people', 'planet', 'city', 'mountain', 'industry', 'computer', 'food','sports', 'forest', 'travel', 'buildings', 'business'];
+//     const datala= await fetch(url+types[count(types.length)]);
+//     const json= await datala.json();
+//     return json.hits[];
+// }
+
+
+//counter
+let add=0
+  const count=(len)=>{
+        if(add<len) add+=1;
+        else add=0;
+        return add;
+  }
